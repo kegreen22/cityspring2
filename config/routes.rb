9d82bd4a2d167d00899a87a41a9fba417902b2ba
Keith Green <kegreen22@gmail.com>
2014-07-11 16:05:32 -0400

    7-11-14 1605

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root :to => "cityspring#home"
   get 'cityspring/results' => 'cityspring#results'
   get 'cityspring/contact' => 'cityspring#contact'
+  get 'cityspring/useful_resources' => 'cityspring#useful_resources'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
