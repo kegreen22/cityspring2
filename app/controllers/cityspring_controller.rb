c7eda012f29d5adc3d6c22fa309cc598b56bc221
Keith Green <kegreen22@gmail.com>
2014-12-10 21:19:14 -0500

    12-10-14 cleanup

--- app/controllers/cityspring_controller.rb
+++ app/controllers/cityspring_controller.rb
@@ -1,52 +1,28 @@
 class CityspringController < ApplicationController
-  def home #show home page
+  def home # show home page
     render "home"
   end
 
-  def index #show home page
+  def index # show home page
     render "home"
   end
 
-def results #current controller for results
+def results # current controller for results
    if params[:search].present? #only capture parameters if something has been entered
          puts params[:search]
          @your_address = params[:search]
      @resources = Resource.near(params[:search], 1, :order => :activity) #use the geocoded address to search the database for locations w/i 1 mile
-     if Resource.geocoded #checking to see that the user input has been geocoded
+     if Resource.geocoded # checking to see that the user input has been geocoded
       puts "geocoded"
       @geocode_true = 1
       puts @geocode_true
     else      
-      puts "not geocoded"  #if user input was not geocoded then note that fact
+      puts "not geocoded"  # if user input was not geocoded then note that fact
       @geocode_true = 0
       puts @geocode_true
      end
 
-     if @resources.empty? #note if that the user input was geocoded but was not near any resources in the database
-       puts "no results"
-       @geocode_true = -1
-       puts @geocode_true
-       end
-    
-   end
-end
-
-def show #original controller for view
-if params[:search].present? #only capture parameters if something has been entered
-         puts params[:search]
-         @your_address = params[:search]
-     @resources = Resource.near(params[:search], 1, :order => :activity) #use the geocoded address to search the database for nearby locations
-     if Resource.geocoded #checking to see that the user input has been geocoded
-      puts "geocoded"
-      @geocode_true = 1
-      puts @geocode_true
-    else      
-      puts "not geocoded"  #if user input was not geocoded then note that fact
-      @geocode_true = 0
-      puts @geocode_true
-     end
-
-     if @resources.empty? #note if that the user input was geocoded but was not near any resources in the database
+     if @resources.empty? # note if that the user input was geocoded but was not near any resources in the database
        puts "no results"
        @geocode_true = -1
        puts @geocode_true
@@ -54,14 +30,13 @@ if params[:search].present? #only capture parameters if something has been enter
     
    end
 
-
 end
 
-def useful_resources #show other nyc agencies and resources for children
+def useful_resources # show other nyc agencies and resources for children
   render "useful_resources"
   end
 
-  def contact_us #page for contacting web developer
+  def contact_us # page for contacting web developer
     render "contact_us"
   end
 
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,5 @@
 class Resource < ActiveRecord::Base
+	validates :address, presence: true  # checking that an address has been entered; there is also HTML5 on client-side checking that field is not blank
 	geocoded_by :address #getting street address with city for geocoding
 	after_validation :geocode, :if => :address_changed?
 end
