713718485da9e21bdd8c4bee5cc9c595061eeea9
Keith Green <kegreen22@gmail.com>
2015-03-08 22:30:50 -0400

    3-8-15 updated error msgs & DRYed up controller

--- app/controllers/cityspring_controller.rb
+++ app/controllers/cityspring_controller.rb
@@ -1,31 +1,33 @@
 class CityspringController < ApplicationController
   def home # show home page
-    render "home"
+    
   end
 
   def index # show home page
-    render "home"
+    
   end
 
 def results # current controller for results
    if params[:search].present? #only capture parameters if something has been entered
-         puts params[:search]
+         #puts params[:search]
          @your_address = params[:search]
      @resources = Resource.near(params[:search], 1, :order => :activity) #use the geocoded address to search the database for locations w/i 1 mile
      if Resource.geocoded # checking to see that the user input has been geocoded
-      puts "geocoded"
+     # puts "geocoded"
       @geocode_true = 1
-      puts @geocode_true
+      # puts @geocode_true
     else      
-      puts "not geocoded"  # if user input was not geocoded then note that fact
+      # puts "not geocoded"  # if user input was not geocoded then note that fact
       @geocode_true = 0
-      puts @geocode_true
+      redirect_to :back, flash: {error: "We are having trouble understanding your address. Please enter a valid address."}
+      # puts @geocode_true
      end
 
      if @resources.empty? # note if that the user input was geocoded but was not near any resources in the database
-       puts "no results"
+       # puts "no results"
        @geocode_true = -1
-       puts @geocode_true
+       redirect_to :back, flash: {error: "Sorry, but there are no NYC resources within one mile of your address. Please enter another address."}
+       # puts @geocode_true
        end
     
    end
@@ -33,11 +35,11 @@ def results # current controller for results
 end
 
 def useful_resources # show other nyc agencies and resources for children
-  render "useful_resources"
+  
   end
 
   def contact_us # page for contacting web developer
-    render "contact_us"
+    
   end
 
 end
